25e9e48198b8d1edcf095384a4da1e1c651ae991
Manik Mia <mohammadmanikmia@yahoo.com>
2023-05-10 14:07:36 +0600

    Edit package.json

--- index.js
+++ index.js
@@ -1,20 +1,10 @@
-// const jsonServer = require("json-server"); // importing json-server library
-// const server = jsonServer.create();
-// const router = jsonServer.router("db.json");
-// const middlewares = jsonServer.defaults();
-// const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
+const jsonServer = require("json-server"); // importing json-server library
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
 
-// server.use(middlewares);
-// server.use(router);
+server.use(middlewares);
+server.use(router);
 
-// server.listen(port);
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
-
-server.use(middlewares)
-server.use(router)
-server.listen(3000, () => {
-  console.log('JSON Server is running')
-})
+server.listen(port);
